713c6258ea2cc8006c939a9ebfa1e9d730d371ab
shubham vaishnav <shubhamvaishnav75486@gmail.com>
2022-08-26 10:02:13 +0530

    BaseClass
    Naukari.com homepage

---
+++ Velocity/src/main/java/ChicwadBatch/Velocity/App.java
@@ -0,0 +1,13 @@
+package ChicwadBatch.Velocity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Velocity/src/main/java/com/qa/testBasse/TestBase.java
@@ -0,0 +1,43 @@
+package com.qa.testBasse;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() {
+		String br = "chrome";
+		if(br.equalsIgnoreCase("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else if(br.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		else {
+			System.out.println("Provide correct browser to launch");
+		}
+		driver.manage().window().maximize();
+		driver.get("https://www.naukri.com/");
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
+}
