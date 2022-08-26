9ed8c21a848408015e15e75fda4de4ede8a05b85
shubham vaishnav <shubhamvaishnav75486@gmail.com>
2022-08-26 10:48:30 +0530

    TestHomePAge
    ForApproval of home page

---
+++ Velocity/src/main/java/com/qa/pageLayers/HomePage.java
@@ -0,0 +1,42 @@
+package com.qa.pageLayers;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.qa.testBasse.TestBase;
+
+public class HomePage extends TestBase{
+	
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "(//input[@class='suggestor-input '])[1]")
+	private WebElement enterskill;
+	public void verifySkills(String skill) {
+		enterskill.sendKeys(skill);
+	}
+	@FindBy(id = "expereinceDD")
+	private WebElement exp;
+	@FindBy(xpath = "(//ul[@class='dropdown ']/li)[4]")
+	private WebElement year;
+	public void verifyYear() {
+		exp.click();
+		year.click();
+		
+	}
+	
+	@FindBy(xpath = "(//input[@class='suggestor-input '])[2]")
+	private WebElement city;
+	public void verifyCity(String name) {
+		city.sendKeys(name);
+	}
+	@FindBy(xpath = "//div[@class='qsbSubmit']")
+	private WebElement search;
+	public void verifySearch() {
+		search.click();
+	}
+	
+}
